2c1a4e429efd80f0e38d01ad1e1556d5b0e6bcfc
Adil Choudhury <a.choudhury4927@gmail.com>
2023-03-16 13:59:26 +0000

    added chainlink vrf contract for random numbers

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,15 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "../node_modules/@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address private immutable i_owner;
     address payable[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_owner = msg.sender;
     }
@@ -20,13 +28,17 @@ contract Raffle {
             revert Raffle__NotEnoughEthEntered();
         }
         s_players.push(payable(msg.sender));
-        // Emite an event when we update a dynamic array or mapping
+        emit RaffleEnter(msg.sender);
     }
 
-    //Pick a random windder (verifiably random)
-    function pickRandomWinner() private view returns (address) {
-        return i_owner;
+    function requestRandomWinner() external {
+        //Request the random number
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     //Winner to be select every x minutes -> completely automate
     //Chainlink Oracle -> Rnaomdness, autoamted exeuction using chainlink keepers
